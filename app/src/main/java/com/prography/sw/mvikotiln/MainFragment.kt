19af9e307d326737fba769dc5fe1c0e5f9143495
SohaybHassan <sohiyp199716@gmail.com>
2021-12-21 14:17:01 +0200

    Initial commit

---
+++ app/src/main/java/com/prography/sw/mvikotiln/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.prography.sw.mvikotiln
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
